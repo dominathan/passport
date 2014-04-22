82cd98e113f4dd7590340f057e9fb3aabc6a70ee
Nathan Hall <nathan.mh@gmail.com>
2014-04-21 21:57:12 -0500

    switching over to mandrill and passportfitnessdc.com

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default from: "nathan.mh@gmail.com"
+  default from: "nathan@passportfitnessdc.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
